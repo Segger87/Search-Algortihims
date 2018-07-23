e742f0b17417705bc67ef2599982910d931dcf38
Segger87 <Segger87@gmail.com>
2018-07-23 09:50:49 +0100

    Created method to download words from source

---
+++ SearchAlgorithms/SearchAlgorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Program();
+			p.DownloadWords();
+		}
+
+		public string[] DownloadWords()
+		{
+			string wordsDownloadedFromSource;
+			using (var webClient = new WebClient())
+			{
+				wordsDownloadedFromSource = webClient.DownloadString(@"https://raw.githubusercontent.com/dwyl/english-words/master/words.txt");
+			}
+
+			var arrayOfDownloadedWords = wordsDownloadedFromSource.Split('\n');
+
+			return arrayOfDownloadedWords;
+
+		}
+	}
+}
